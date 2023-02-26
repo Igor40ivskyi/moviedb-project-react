2521138cef8e7bfaca67cd36c6448aff172d9916
Admin <sorokivskyi777@gmail.com>
2023-02-26 10:36:48 +0200

    do the toggle

--- src/components/PosterPreviewByGenreId/PosterPreviewByGenreId.js
+++ src/components/PosterPreviewByGenreId/PosterPreviewByGenreId.js
@@ -1,11 +1,13 @@
 
+import '../OneMovieByGenreId/OneMovieByGenreId.css'
+
 const PosterPreviewByGenreId = ({poster_path}) => {
 
     const fullPoster = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
     return (
         <div>
-            <img style={{height:350,width:250}} src={fullPoster} alt="moviePoster"/>
+            <img className={'picture'} src={fullPoster} alt="moviePoster"/>
         </div>
 
     );
